1623949b40b2afab1946e6e3c16afe094ca4f48b
tomisteven <tomssteven22@gmail.com>
2026-07-01 17:18:24 -0300

    log

--- Routes/Log.router.js
+++ Routes/Log.router.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { getLogs, deleteLogs } = require("../controllers/Log.controller");
+const md_auth = require("../middlewares/authenticated");
 
-router.get("/logs", getLogs);
-router.delete("/logs", deleteLogs);
+router.get("/logs", [md_auth.asureAuth], getLogs);
+router.delete("/logs", [md_auth.asureAuth], deleteLogs);
 
 module.exports = router;
+
